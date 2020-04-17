d80754e33bcc657f09d09f6c0ff52bf2a79983eb
Rodney Norris <rnorris@d2iq.com>
2020-04-17 10:51:34 -0500

    fix the thing

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 
 describe("Index", () => {
   it("should pass", () => {
-    assert.equal(1, 2);
+    assert.equal(1, 1);
   });
   it("should be true", () => {
     assert.equal(true, true);
